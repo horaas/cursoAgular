6852d7bd516faf69d135e2520868b20fb4ccbe98
horaas <shoracioa@gmail.com>
2018-10-06 22:37:55 -0500

    Actualización de proyecto

--- src/app/servicio.service.ts
+++ src/app/servicio.service.ts
@@ -8,16 +8,16 @@ export class ServicioService {
 
   private equipo: any[] = [
     {
-      nombre: "juan Alberto",
-      especialidad: "HTML",
-      descripcion: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed veniam illum recusandae blanditiis adipisci, quis eaque dolores dolor et libero fugiat iste officiis soluta quo architecto porro molestiae? Dolor, cupiditate!"
+      nombre: 'juan Alberto',
+      especialidad: 'HTML',
+      descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed veniam illum recusandae blanditiis adipisci, quis eaque dolores dolor et libero fugiat iste officiis soluta quo architecto porro molestiae? Dolor, cupiditate!'
     },
     {
-      nombre: "Maria",
-      especialidad: "CSS",
-      descripcion: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed voluptas maxime molestiae consectetur quas vel illum repellat, quos odit magnam, doloribus id porro cupiditate. Consequuntur alias labore quisquam nemo atque?"
+      nombre: 'Maria',
+      especialidad: 'CSS',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed voluptas maxime molestiae consectetur quas vel illum repellat, quos odit magnam, doloribus id porro cupiditate. Consequuntur alias labore quisquam nemo atque?'
     }
-  ]
+  ];
   constructor(private http: HttpClient) {
     this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((res) => {
       console.dir(res);
